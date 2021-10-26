38c6596cd9ee8ef66fe24acc6742c556d2d12bdc
hazelbarcibal <hazelannmae.barcibal@gsfe.tupcavite.edu.ph>
2021-10-26 20:38:55 +0800

    django_proj
    barci-update-1

--- django_proj/django_proj/urls.py
+++ django_proj/django_proj/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('violation/', include('violation.urls')),
+    path('', include('violation.urls')),
 ]
