1f5e95b681bf57b29b2c84930a9fd6a4fbadc3d0
Marco Paolo Asuncion <marcopaolo.asuncion@gsfe.tupcavite.edu.ph>
2021-10-26 22:44:19 +0800

    violation
    hayss

--- django_proj/violation/urls.py
+++ django_proj/violation/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='violation-home'),
     path('login/', views.login, name='violation-login'),
-    path('innerpage/', views.innerpage, name='violation-innerpage'),
+    path('innerpage/', views.innerpage, name='violation-innerpage')
 ]
